e6562ac1d7fa85fe10d8540139b34bd6e2a65fe5
Patrik Hermansson <hermansson.patrik@gmail.com>
2023-12-17 19:45:43 +0100

    Working on price algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,18 @@ TFT_eSPI tft = TFT_eSPI();
 WiFiClient wifiClient;
 
 unsigned long previousMillis = 0;
-const long interval = 8000; 
-StaticJsonDocument<1024> doc;
+const long interval = 80000; 
+StaticJsonDocument<3072> doc;
 float curPrices[24];
+
+struct allPricesStruct {
+  float price;
+  int hour;
+};
+
+void getPrices();
+void sort(int n, allPricesStruct* ptr);  
+
 
 void setup() {
   tft.init();
@@ -48,11 +57,13 @@ void setup() {
   tft.println("Time ok");
   delay(2500);
   tft.fillRect(0,0,320,200,0x000); // 320 = width
+
+  getPrices();
 }
 
 void loop() {
   long int ms = millis();
-  tft.fillRect(0,0,320,20,0x000); // 320 = width
+  tft.fillRect(0,0,320,30,0x000); // 320 = width
   tft.setCursor(11, 11, 4);
 
   char *datetime_str = &datetime_buf[0];
@@ -60,16 +71,15 @@ void loop() {
   datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
   char curTime[25];
   sprintf(curTime, "%02d:%02d:%02d", t.hour, t.min, t.sec);
-
   tft.println(curTime);
   delay(1000);
 
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
-    char curYear[5], curMonth[5], curDay[5];  
-    sprintf(curYear, "%02d", t.year);
-    sprintf(curMonth, "%02d", t.month);
-    sprintf(curDay, "%02d", t.day);
+    // char curYear[5], curMonth[5], curDay[5];  
+    // sprintf(curYear, "%02d", t.year);
+    // sprintf(curMonth, "%02d", t.month);
+    // sprintf(curDay, "%02d", t.day);
     // char hostName[] = HOST_NAME;
     // int httpPort = HTTP_PORT;
     // char seZone[6] = SEZONE;
@@ -88,34 +98,133 @@ void loop() {
     // strcat(curDate, ".json");
     // strcat(hostName, curDate);
 
-    //char question[100] = "{\"query\": \"{ viewer { name }}\"}";
-    char question[200] = "{\"query\": \"{ viewer {homes {currentSubscription {priceInfo {today {total energy tax startsAt }}}}}}\"}";
-
-    //String json = webserverRequest(question); // Demands more memory due to duplication
-    const char * json = webserverRequest(question);
-    tft.println(json);
-
-    // We get an array with several objects [] = array, {} = object
-    // Ref: https://arduinojson.org/v6/assistant
-    DeserializationError error = deserializeJson(doc, json);
-
-    tft.println("Got data");
-    if (error) {
-      tft.println("deserializeJson() misslyckades: ");
-      tft.println(error.c_str());
-
-    }
+    
     // Hämta data från JSON-dokumentet
-    float total = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][0]["total"]; // 1.3642
-    float energy = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][0]["energy"]; // 0.9938
-    float tax = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][0]["tax"]; // 0.3704
-    const char* startsAt = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][0]["startsAt"]; 
-    tft.println("Total: ");
-    total = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][1]["total"];
-    tft.println("Total: ");
+    // float total = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][0]["total"]; // 1.3642
+    // float energy = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][0]["energy"]; // 0.9938
+    // float tax = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][0]["tax"]; // 0.3704
+    // const char* startsAt = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][0]["startsAt"]; 
+//    tft.print("Total: ");
+//    tft.println(total);
+
+
+
+
+    // float curPrice = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][t.hour]["total"];
+    // tft.print("Now: ");
+    // tft.println(curPrice);
+
+
 
 
     previousMillis = currentMillis;
   }
 
 }
+
+void getPrices(){
+    char questionToday[200] = "{\"query\": \"{ viewer {homes {currentSubscription {priceInfo {today {total energy tax startsAt }}}}}}\"}";
+
+    // Ref: https://arduinojson.org/v6/assistant
+
+    DeserializationError error = deserializeJson(doc, webserverRequest(questionToday));
+    tft.println("Got data for today");
+    if (error) {
+      tft.println("deserializeJson() misslyckades: ");
+      tft.println(error.c_str());
+    }
+
+    
+
+    allPricesStruct allPrices[48];
+
+    int j;
+    for(j=0;j<24;j++){
+      allPrices[j].price = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][j]["total"];
+      String time = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][j]["startsAt"];
+        char buf[11];
+        time.toCharArray(buf, 25);
+        char* ptr = strtok(buf, "T");
+        ptr = strtok(NULL, ":");
+        allPrices[j].hour = atoi(ptr);
+    }
+
+    // char questionTomorrow[200] = "{\"query\": \"{ viewer {homes {currentSubscription {priceInfo {tomorrow {total energy tax startsAt }}}}}}\"}";
+    // error = deserializeJson(doc, webserverRequest(questionTomorrow));
+    // tft.println("Got data for tomorrow");
+    // if (error) {
+    //   tft.println("deserializeJson() misslyckades: ");
+    //   tft.println(error.c_str());
+    // }
+
+    // int c=0;
+    // for(j=j;j<48;j++){
+    //   allPrices[j] = doc["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"][j]["total"];
+    //   if (allPrices[j].price == 0) {
+    //     c++;
+    //   }
+    // }
+    // tft.println(c);
+
+
+     allPricesStruct *ptrValues;
+     ptrValues = allPrices;
+
+      sort(10, ptrValues);  
+      tft.print(ptrValues[0].hour);
+      tft.println(ptrValues[0].price);
+
+    // for(int k=0;k<48;k++){
+    //   //tft.println(allPrices[k]);
+    // }
+
+    // int cheapTime=0;
+    // int cheapestHour=allPrices[0];
+    // float values[48];
+    // for(int i = 0; i<30; i++) { // Do we always have 20 values? No.
+
+    //   if(allPrices[i] < cheapestHour) {
+    //     cheapestHour = allPrices[i];
+    //   }
+
+    //   float a = allPrices[i]; 
+    //   float b = allPrices[i+1];
+    //   float c = allPrices[i+2];
+    //   if(a>0 && b > 0 && c>0) {
+    //     float meanValue = (a+b+c)/3;
+    //       if (meanValue<1) {
+    //         cheapTime = i;
+    //         break;
+    //       }
+    //   }
+    // }
+    // if(cheapTime>0) {
+    //     tft.println(cheapTime);
+    // }
+    // else {
+    //   tft.println("No cheap time today");
+    //   tft.println("Cheapest: ");
+    //   tft.println(cheapestHour);
+    // }
+}
+void sort(int n,  allPricesStruct *allPrices)  
+{  
+    int i, j, t;  
+    float *ptr;
+    float price = allPrices->price;
+    *ptr = price;
+    // Sort the numbers using pointers  
+    for (i = 0; i < n; i++) {  
+        for (j = i + 1; j < n; j++) {  
+            if (*(ptr + j) < *(ptr + i)) {  
+                t = *(ptr + i);  
+                *(ptr + i) = *(ptr + j);  
+                *(ptr + j) = t;  
+            }  
+        }  
+    }  
+  
+    // print the numbers  
+    // for (i = 0; i < n; i++)  
+    //     printf("%d ", *(ptr + i));  
+}  
